b856f50fde16bf34c393fbe9e5fd3d355cee20e8
barath923 <124130166+barath923@users.noreply.github.com>
2023-11-02 13:42:27 +0530

    Delete .github/workflows/resource_group directory

--- .github/workflows/resource_group/rg.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_resource_group" "ht_rg" {
-  name     = "ht-terraform"
-  location = var.location
-}
-
-output "resource_group_name" {
-  value = azurerm_resource_group.ht_rg.name
-}
