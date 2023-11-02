d147b509e4154ab1f12955fcd968c2d97a0b6b8d
barath923 <124130166+barath923@users.noreply.github.com>
2023-11-02 13:43:26 +0530

    Delete .github/workflows/storage_acc directory

--- .github/workflows/storage_acc/sa.tf
+++
@@ -1,8 +0,0 @@
-# Storage Account creation
-resource "azurerm_storage_account" "ht_stgacc" {
-  name                     = "mystorageaccountht"
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
